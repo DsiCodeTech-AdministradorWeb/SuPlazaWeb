2f76db16a964fcd815ec09edb9c4859792e4d42d
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-07 21:43:35 -0500

    se creo la clase Unidad de trabajo, sin  incorporar aun el contexto de datos.

---
+++ DsiCodetech.Administrador.Repository/Infraestructure/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository.Infraestructure
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        //private readonly IncubadoraDataBaseEntities _dbContext;
+        
+        public UnitOfWork()
+        {
+          //  _dbContext = new IncubadoraDataBaseEntities();
+        }
+
+        public DbContext Db
+        {
+            get { return _dbContext; }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
