6128f6b2b61b3372b3d69a874f00aed63bee5377
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:38:41 -0500

    se agrego la capa de negocios

---
+++ DsiCodetech.Administrador.Business/PaisBusiness.cs
@@ -0,0 +1,42 @@
+﻿using DsiCodetech.Administrador.Business.Interface;
+using DsiCodetech.Administrador.Domain;
+using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class PaisBusiness: IPaisBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Pais_Repository repository;
+        public PaisBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Pais_Repository(unitOfWork);
+        }
+
+        /// <summary>
+        /// Este metodo se encarga de consultar todos los paises almacenados en la persistencia
+        /// </summary>
+        /// <returns>una lista de paises</returns>
+        public List<PaisDM> GetPaises() 
+        {
+            List<PaisDM> paises = null;
+            return paises = repository.GetAll().Select(p => new PaisDM
+            {
+                Id =p.id,
+                Agrupacion = p.agrupacion,
+                Descripcion =p.descripcion
+
+            }).ToList();
+        }
+
+        
+
+    }
+}
