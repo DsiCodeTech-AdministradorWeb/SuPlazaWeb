126c18731528c73a2f1ddf5a2d03f5a39cc94351
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:00:10 -0500

    Se agrego moneda y mes domainmodel

---
+++ DsiCodetech.Administrador.Domain/MonedaDM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class MonedaDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Decimales { get; set; }
+        public string Porcentaje { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
