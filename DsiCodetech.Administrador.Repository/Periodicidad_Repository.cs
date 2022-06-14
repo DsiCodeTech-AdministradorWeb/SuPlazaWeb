b5ee1c6243cc7b98888ce1df6ee9f2596febec84
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 00:39:55 -0500

    se agrego periocidad repository

---
+++ DsiCodetech.Administrador.Repository/Periodicidad_Repository.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository
+{
+    public class Periodicidad_Repository: BaseRepository<periodicidad>
+    {
+        public Periodicidad_Repository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
