d424d2830c4f5e736547b25dd3ea8325d39c7e3b
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:08:09 -0500

    Se agrego MeotodoPagoDM

---
+++ DsiCodetech.Administrador.Domain/MetodoPagoDM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class MetodoPagoDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
