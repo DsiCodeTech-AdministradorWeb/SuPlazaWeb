b4544211bc9cba5214ca3484d5d518b9a2343036
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:16:47 -0500

    Se agrego UsoCfdiDM

---
+++ DsiCodetech.Administrador.Domain/RegimenFiscalDM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class RegimenFiscalDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Persona_Fisica { get; set; }
+        public string Persona_Moral { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
---
+++ DsiCodetech.Administrador.Domain/UsoCfdiDM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+   public class UsoCfdiDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Persona_Fisica { get; set; }
+        public string Persona_Moral { get; set; }
+        public string Regimen_Fiscal_Receptor { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
