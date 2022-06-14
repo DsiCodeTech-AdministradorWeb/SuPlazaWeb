7d20b4df50894e6043a8227a0fc6d55e248eccdd
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:12:17 -0500

    se agrego periocidadDM

---
+++ DsiCodetech.Administrador.Domain/PeriocidadDM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class PeriocidadDM
+    {
+        public string Id
+        {
+            get; set;
+        }
+        public string Descripcion
+        {
+            get; set;
+        }
+        public DateTime Fecha_Inicio
+        {
+            get; set;
+        }
+        public DateTime Fecha_Fin
+        {
+            get; set;
+        }
+     }
+}
