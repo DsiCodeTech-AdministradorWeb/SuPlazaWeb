dda92e165852befab034095aad7faa52fb81848f
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 00:32:45 -0500

    se agrego metodo de pago repository

---
+++ DsiCodetech.Administrador.Repository/Metodo_Pago_Repository.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository
+{
+    public class Metodo_Pago_Repository : BaseRepository<metodo_pago>
+    {
+        public Metodo_Pago_Repository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
