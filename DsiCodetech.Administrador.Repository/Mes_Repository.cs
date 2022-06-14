e4832ba194765c59db323ecf9b589c9ad606dde0
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 00:24:24 -0500

    se agrego mes repository

---
+++ DsiCodetech.Administrador.Repository/Mes_Repository.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository
+{
+    public class Mes_Repository: BaseRepository<mes>
+    {
+        public Mes_Repository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
