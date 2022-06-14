76b26593e6c13a30574323f5c0ae1f928f167728
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 00:37:57 -0500

    se agrego tipo de lreacion repository

---
+++ DsiCodetech.Administrador.Repository/Tipo_Relacion_Repository.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository
+{
+    public class Tipo_Relacion_Repository: BaseRepository<tipo_relacion>
+    {
+        public Tipo_Relacion_Repository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
