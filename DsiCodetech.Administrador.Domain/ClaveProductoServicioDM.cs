3c43b771072be4d5b4c90673466d65a8c9f78c7a
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 00:53:54 -0500

    se agregaron clases del tipo domain model

---
+++ DsiCodetech.Administrador.Domain/ClaveProductoServicioDM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class ClaveProductoServicioDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Trasladado { get; set; }
+        public string Ieps_trasladado { get; set; }
+        public short Franja_fronteriza { get; set; }
+        public string Palabras_similares { get; set; }
+        public DateTime Fecha_inicio { get; set; }
+        public DateTime Fecha_fin { get; set; }
+    }
+}
---
+++ DsiCodetech.Administrador.Repository/Uso_Cfdi_Repository.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Repository
+{
+    public class Uso_Cfdi_Repository: BaseRepository<uso_cfdi>
+    {
+        public Uso_Cfdi_Repository(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+    }
+}
