18ee26a133f678d53c260ee862da2fa544e4774d
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:47:57 -0500

    se agrega la interfaz con la implementacion de monedaBusiness

---
+++ DsiCodetech.Administrador.Business/Interface/IMonedaBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IMonedaBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar las monedas
+        /// </summary>
+        /// <returns>una coleccion de monedas domain model</returns>
+        List<MonedaDM> GetMonedas();
+    }
+}
