5a6ec6610fd005f1683a48b98cf6ff7ff13f0649
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:42:49 -0500

    se agrego el metodo de negocio MonedaBusiness

---
+++ DsiCodetech.Administrador.Business/MonedaBusiness.cs
@@ -0,0 +1,21 @@
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class MonedaBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Moneda_Repository repository;
+        public MonedaBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Moneda_Repository(unitOfWork);
+        }
+    }
+}
