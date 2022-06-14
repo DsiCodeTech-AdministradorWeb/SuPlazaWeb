18d4770315e7093ffbd65440c69aa63d979225f6
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 01:05:44 -0500

    Se agrego FormaPagoDM

---
+++ DsiCodetech.Administrador.Domain/FormaPagoDM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Domain
+{
+    public class FormaPagoDM
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Bancarizado { get; set; }
+        public string Num_Operacion { get; set; }
+        public string Rfc_Ordenante { get; set; }
+        public string Cuenta_Ordenante { get; set; }
+        public string Rfc_Beneficiario { get; set; }
+        public string Cuenta_Beneficiario { get; set; }
+        public string Tipo_Pago { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
