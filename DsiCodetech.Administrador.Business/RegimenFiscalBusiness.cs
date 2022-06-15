4c3709ce782968864726bb0a20e6f5ff1501474c
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 20:01:38 -0500

    se agrego la clase regimenfiscalbusiness con el metodod e consulta general.

---
+++ DsiCodetech.Administrador.Business/RegimenFiscalBusiness.cs
@@ -0,0 +1,36 @@
+﻿using DsiCodetech.Administrador.Domain;
+using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class RegimenFiscalBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Regimen_Fiscal_Repository repository;
+        public RegimenFiscalBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+        /// <summary>
+        /// Este metodo se encarga de consultar todos los regimenes fiscales
+        /// </summary>
+        /// <returns>una lista de regimen fiscal</returns>
+        public List<RegimenFiscalDM> GetAllRegimenFiscal() {
+            return repository.GetAll().Select(p => new RegimenFiscalDM
+            {
+                Id = p.id,
+                Descripcion = p.descripcion,
+                Fecha_Inicio = p.fecha_inicio.Value,
+                Fecha_Fin = p.fecha_fin.Value,
+                Persona_Fisica = p.persona_fisica,
+                Persona_Moral = p.persona_moral
+            }).ToList();
+        }
+    }
+}
