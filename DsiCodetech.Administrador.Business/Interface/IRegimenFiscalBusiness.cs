17e9514192f16314ed45954ff677b7fb476c5c72
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 20:17:32 -0500

    se agrego el emtodo de busqueda de regimen fiscal por id

---
+++ DsiCodetech.Administrador.Business/Interface/IRegimenFiscalBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IRegimenFiscalBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar todos los regimenes fiscales
+        /// </summary>
+        /// <returns>una lista de regimen fiscal</returns>
+        List<RegimenFiscalDM> GetAllRegimenFiscal();
+    }
+}
--- DsiCodetech.Administrador.Business/RegimenFiscalBusiness.cs
+++ DsiCodetech.Administrador.Business/RegimenFiscalBusiness.cs
@@ -1,4 +1,5 @@
-﻿using DsiCodetech.Administrador.Domain;
+﻿using DsiCodetech.Administrador.Business.Interface;
+using DsiCodetech.Administrador.Domain;
 using DsiCodetech.Administrador.Repository;
 using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
 using System;
@@ -9,13 +10,14 @@ using System.Threading.Tasks;
 
 namespace DsiCodetech.Administrador.Business
 {
-    public class RegimenFiscalBusiness
+    public class RegimenFiscalBusiness : IRegimenFiscalBusiness
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly Regimen_Fiscal_Repository repository;
         public RegimenFiscalBusiness(IUnitOfWork _unitOfWork)
         {
             unitOfWork = _unitOfWork;
+            repository = new Regimen_Fiscal_Repository(unitOfWork);
         }
         /// <summary>
         /// Este metodo se encarga de consultar todos los regimenes fiscales
@@ -32,5 +34,25 @@ namespace DsiCodetech.Administrador.Business
                 Persona_Moral = p.persona_moral
             }).ToList();
         }
+
+        /// <summary>
+        /// Este metodo se encargad e hacer una consulta por Id
+        /// </summary>
+        /// <param name="id">el identificador de la entidad</param>
+        /// <returns>regresa una sola entidad</returns>
+        public RegimenFiscalDM GetRegimenFiscalById(string id) {
+
+            var regimen= repository.SingleOrDefault(p => p.id == id);
+            RegimenFiscalDM regimenFiscal = new RegimenFiscalDM { 
+                Id=regimen.id,
+                Descripcion = regimen.descripcion,
+                Fecha_Inicio = regimen.fecha_inicio.Value,
+                Fecha_Fin = regimen.fecha_fin.Value,
+                Persona_Fisica = regimen.persona_fisica,
+                Persona_Moral = regimen.persona_moral
+            };
+            return regimenFiscal;
+        }
+
     }
 }
