37eb6d1d75b61ae9cd101a4fd5a7c2c12513b813
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-14 20:53:10 -0500

    se genero el mapeo de la entidad regimenfiscaldm a regimenfiscaldto

---
+++ DsiCodetech.Administrador.Web/Dto/RegimenFiscalDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DsiCodetech.Administrador.Web.Dto
+{
+    public class RegimenFiscalDto
+    {
+        public string Id { get; set; }
+        public string Descripcion { get; set; }
+        public string Persona_Fisica { get; set; }
+        public string Persona_Moral { get; set; }
+        public DateTime Fecha_Inicio { get; set; }
+        public DateTime Fecha_Fin { get; set; }
+    }
+}
---
+++ DsiCodetech.Administrador.Web/Infraestructure/AutomaperWebProfile.cs
@@ -0,0 +1,17 @@
+﻿using DsiCodetech.Administrador.Domain;
+using DsiCodetech.Administrador.Web.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DsiCodetech.Administrador.Web.Infraestructure
+{
+    public class AutomaperWebProfile: AutoMapper.Profile
+    {
+        public AutomaperWebProfile()
+        {
+            CreateMap<RegimenFiscalDM, RegimenFiscalDto>().ReverseMap();
+        }
+    }
+}
