0c6e23c06ae01ec6c23d861ca218f50fd0095333
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-19 01:53:50 -0500

    se agrego la clase base del tipo tiporelacionbusiness

---
+++ DsiCodetech.Administrador.Business/TipoRelacionBusiness.cs
@@ -0,0 +1,21 @@
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class TipoRelacionBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Tipo_Relacion_Repository repository;
+        public TipoRelacionBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Tipo_Relacion_Repository(unitOfWork);
+        }
+    }
+}
