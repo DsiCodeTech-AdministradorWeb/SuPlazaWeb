b46927220ab8cb3444b62e66c8af283c7d25fe21
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-19 00:14:09 -0500

    se agrego los atributos de logger de el web api de regimen fiscal.

--- DsiCodetech.Administrador.Web/Controllers/RegimenFiscalController.cs
+++ DsiCodetech.Administrador.Web/Controllers/RegimenFiscalController.cs
@@ -1,5 +1,6 @@
 ﻿using DsiCodetech.Administrador.Business.Interface;
 using DsiCodetech.Administrador.Web.Resources;
+using NLog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,9 @@ namespace DsiCodetech.Administrador.Web.Controllers
     public class RegimenFiscalController : ApiController
     {
         private readonly IRegimenFiscalBusiness regimenFiscalBusiness;
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+        private static readonly Logger loggerdb = LogManager.GetLogger("databaseLogger");
+
         public RegimenFiscalController(IRegimenFiscalBusiness _regimenFiscalBusiness)
         {
             regimenFiscalBusiness = _regimenFiscalBusiness;
@@ -35,6 +39,8 @@ namespace DsiCodetech.Administrador.Web.Controllers
             }
             catch (Exception ex)
             {
+                Log.Error(ex, "Error en la peticion dentro de: RegimenFiscal, en la Acción : GetRegimenFiscal");
+                loggerdb.Error(ex);
                 return BadRequest(Utilerias.BAD_REQUEST);
                 
             }
