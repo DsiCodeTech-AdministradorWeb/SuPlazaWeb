38951c61bbd0c4779c601c5c37bb4d3505100856
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-19 20:14:51 -0500

    se agrego la interfaz de codigo con la implementacion de metodos  la clase

---
+++ DsiCodetech.Administrador.Business/Interface/IFormaPagoBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IFormaPagoBusiness
+    {
+        /// <summary>
+        /// este metodo se encarga de consultar todas las formas de pago
+        /// </summary>
+        /// <returns>regresa una coleccion de todas las formas de pago</returns>
+        List<FormaPagoDM> GetAllFormasDePago();
+    }
+}
