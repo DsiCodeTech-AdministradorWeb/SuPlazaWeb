cbd826117b6c4442a4c91e26acaead9a2267a2d4
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-19 20:01:49 -0500

    se agrego  los elementos clave de la clase forma de pago business

---
+++ DsiCodetech.Administrador.Business/FormaPagoBusiness.cs
@@ -0,0 +1,22 @@
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class FormaPagoBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Forma_Pago_Repository repository;
+        public FormaPagoBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Forma_Pago_Repository(unitOfWork);
+        }
+
+    }
+}
