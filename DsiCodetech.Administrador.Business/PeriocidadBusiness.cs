d4a67b89c677a0c2195480721416dd5d324e9f2a
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-21 21:17:23 -0500

    se genero metodo de negocio PeriocidadBusiness con los elementos base

---
+++ DsiCodetech.Administrador.Business/PeriocidadBusiness.cs
@@ -0,0 +1,21 @@
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class PeriocidadBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Periodicidad_Repository repository;
+        public PeriocidadBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Periodicidad_Repository(unitOfWork);
+        }
+    }
+}
