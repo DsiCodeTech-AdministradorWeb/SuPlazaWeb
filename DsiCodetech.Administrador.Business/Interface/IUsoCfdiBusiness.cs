e0f29398fab18869064d3a3648024335eade0cce
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-21 21:36:23 -0500

    se agrego la interfaz de usocfdiBusiness

---
+++ DsiCodetech.Administrador.Business/Interface/IUsoCfdiBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IUsoCfdiBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar todos los UsoCfdi en la base de datos
+        /// </summary>
+        /// <returns>una coleccion de UsoCfdi</returns>
+        List<UsoCfdiDM> GetAllUsoCfdi();
+    }
+}
