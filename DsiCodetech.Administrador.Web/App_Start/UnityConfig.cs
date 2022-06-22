1fbc3855703951217aa730f5abee77aae744afbb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-22 17:50:23 -0500

    Se configuro el MesBusiness en UniyConfig

--- DsiCodetech.Administrador.Web/App_Start/UnityConfig.cs
+++ DsiCodetech.Administrador.Web/App_Start/UnityConfig.cs
@@ -27,6 +27,7 @@ namespace DsiCodetech.Administrador.Web
             container.RegisterType<IClaveProductoServiciosBusiness,ClaveProductoServiciosBusiness>();
             container.RegisterType<IPeriocidadBusiness,PeriocidadBusiness>();
             container.RegisterType<IUsoCfdiBusiness, UsoCfdiBusiness>();
+            container.RegisterType<IMesBusiness, MesBusiness>();
             // e.g. container.RegisterType<ITestService, TestService>();
             DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
             GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
