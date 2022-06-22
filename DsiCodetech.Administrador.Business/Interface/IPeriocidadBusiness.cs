cb33f3261b3721dd7d78d5fb5b4b29da97c54461
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-21 21:23:35 -0500

    se implemento  la interfaz de periocidadBusiness

---
+++ DsiCodetech.Administrador.Business/Interface/IPeriocidadBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IPeriocidadBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar todas las periocidades
+        /// </summary>
+        /// <returns>una coleccion de periocidadesDM</returns>
+        List<PeriocidadDM> GetAllPeriocidades();
+    }
+}
