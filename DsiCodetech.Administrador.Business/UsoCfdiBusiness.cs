f9af69086c0a4b138c97063ab693d40940f134be
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-21 21:29:04 -0500

    se agrego la clase UsoCfdiBusiness base con los elementos esenciales de trabajo

---
+++ DsiCodetech.Administrador.Business/UsoCfdiBusiness.cs
@@ -0,0 +1,24 @@
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class UsoCfdiBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly Uso_Cfdi_Repository repository;
+        public UsoCfdiBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+            repository = new Uso_Cfdi_Repository(unitOfWork);
+        }
+
+
+
+    }
+}
