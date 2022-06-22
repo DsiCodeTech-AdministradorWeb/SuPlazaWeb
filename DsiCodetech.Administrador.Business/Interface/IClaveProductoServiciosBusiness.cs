86c5d61e5ccc1f5908a294240d70f45edf216dfd
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-21 21:01:29 -0500

    se agrego la interfaz de ClaveProductoServicioBusiness con todas sus implementaciones

---
+++ DsiCodetech.Administrador.Business/Interface/IClaveProductoServiciosBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IClaveProductoServiciosBusiness
+    {
+        /// <summary>
+        /// este metodo se encarga de consultar todas las claves de productosservicioDM
+        /// </summary>
+        /// <returns>una coleccion de clavesProductoServicio</returns>
+        List<ClaveProductoServicioDM> GetAllClaveProductoServicio();
+    }
+}
