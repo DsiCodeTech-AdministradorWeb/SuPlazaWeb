5ec9db87605c6b9b7e22531bb81421335d598f23
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-23 20:48:32 -0500

    se creo la clase clienteRepository

--- DsiCodetech.Administrador.Business/ClienteBusiness.cs
+++ DsiCodetech.Administrador.Business/ClienteBusiness.cs
@@ -1,4 +1,5 @@
-﻿using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+﻿using DsiCodetech.Administrador.Repository;
+using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +11,11 @@ namespace DsiCodetech.Administrador.Business
     public class ClienteBusiness
     {
         private readonly IUnitOfWork unitOfWork;
+        private readonly ClienteRepository repository;
         public ClienteBusiness(IUnitOfWork _unitOfWork)
         {
             unitOfWork = _unitOfWork;
+
         }
     }
 }
