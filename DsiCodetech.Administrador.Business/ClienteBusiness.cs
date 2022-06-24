d1fa686bf93c0b65970d3ed2dfa7738e82b94bcb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-24 00:43:29 -0500

    se documento el metodo de busqueda  GetClientePorId

--- DsiCodetech.Administrador.Business/ClienteBusiness.cs
+++ DsiCodetech.Administrador.Business/ClienteBusiness.cs
@@ -14,12 +14,18 @@ namespace DsiCodetech.Administrador.Business
     {
         private readonly IUnitOfWork unitOfWork;
         private readonly ClienteRepository repository;
+
         public ClienteBusiness(IUnitOfWork _unitOfWork)
         {
             unitOfWork = _unitOfWork;
             repository = new ClienteRepository(unitOfWork);
         }
 
+        /// <summary>
+        /// Este metodo se encarga de consultar un cliente por Identificador
+        /// </summary>
+        /// <param name="id">identificador de la entidad</param>
+        /// <returns>la entidad del tipo clienteDM</returns>
         public ClienteDM GetClientePorId(string id)
         {
 
