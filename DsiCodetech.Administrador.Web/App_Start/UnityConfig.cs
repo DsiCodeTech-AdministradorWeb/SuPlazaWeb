bcd20d7e24fca1a0e5a8b2dbd6f1a3e35f852452
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-24 00:53:16 -0500

    se genero la modificacion la clase unity

--- DsiCodetech.Administrador.Web/App_Start/UnityConfig.cs
+++ DsiCodetech.Administrador.Web/App_Start/UnityConfig.cs
@@ -28,6 +28,7 @@ namespace DsiCodetech.Administrador.Web
             container.RegisterType<IPeriocidadBusiness,PeriocidadBusiness>();
             container.RegisterType<IUsoCfdiBusiness, UsoCfdiBusiness>();
             container.RegisterType<IMesBusiness, MesBusiness>();
+            container.RegisterType<IClienteBusiness, ClienteBusiness>();
             // e.g. container.RegisterType<ITestService, TestService>();
             DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
             GlobalConfiguration.Configuration.DependencyResolver = new Unity.WebApi.UnityDependencyResolver(container);
