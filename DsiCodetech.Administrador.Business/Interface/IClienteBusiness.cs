7d9e6d4670c02fce34d88d86e81ad17e96ec5c72
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-24 00:48:43 -0500

    se agrego la implementacion de la interfaz

---
+++ DsiCodetech.Administrador.Business/Interface/IClienteBusiness.cs
@@ -0,0 +1,19 @@
+﻿using DsiCodetech.Administrador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business.Interface
+{
+    public interface IClienteBusiness
+    {
+        /// <summary>
+        /// Este metodo se encarga de consultar un cliente por Identificador
+        /// </summary>
+        /// <param name="id">identificador de la entidad</param>
+        /// <returns>la entidad del tipo clienteDM</returns>
+        ClienteDM GetClientePorId(string id);
+    }
+}
