14a45bf2883c503dcf3b493de419a734e9577bdb
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-23 20:55:53 -0500

    se agrego el metodo de consulta por Id de Cliente

--- DsiCodetech.Administrador.Business/ClienteBusiness.cs
+++ DsiCodetech.Administrador.Business/ClienteBusiness.cs
@@ -1,4 +1,5 @@
-﻿using DsiCodetech.Administrador.Repository;
+﻿using DsiCodetech.Administrador.Domain;
+using DsiCodetech.Administrador.Repository;
 using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
 using System;
 using System.Collections.Generic;
@@ -16,6 +17,26 @@ namespace DsiCodetech.Administrador.Business
         {
             unitOfWork = _unitOfWork;
 
+        }
+
+        public ClienteDM GetClientePorId(string id)
+        {
+
+            var clienteModel = repository.SingleOrDefault(p => p.id == id);
+            ClienteDM metodoPagoModel = new ClienteDM
+            {
+                IdCliente = clienteModel.id_cliente,
+                Rfc = clienteModel.rfc,
+                Razon_Social = clienteModel.razon_social,
+                Contacto = clienteModel.contacto,
+                Email = clienteModel.e_mail,
+                Email2 = clienteModel.e_mail2,
+                IdMunicipio = clienteModel.id_municipio,
+                IdEntidad= clienteModel.id_entidad
+                
+
+            };
+            return metodoPagoModel;
         }
     }
 }
