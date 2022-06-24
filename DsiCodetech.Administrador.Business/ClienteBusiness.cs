7311d5fccc0c50a97daeb93d5e50813acd900d71
Erik Guerrero Bravo <eguerrero@uttt.edu.mx>
2022-06-23 20:46:26 -0500

    se agrego la clase base de negocios de clienteBusiness

---
+++ DsiCodetech.Administrador.Business/ClienteBusiness.cs
@@ -0,0 +1,18 @@
+﻿using DsiCodetech.Administrador.Repository.Infraestructure.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DsiCodetech.Administrador.Business
+{
+    public class ClienteBusiness
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public ClienteBusiness(IUnitOfWork _unitOfWork)
+        {
+            unitOfWork = _unitOfWork;
+        }
+    }
+}
